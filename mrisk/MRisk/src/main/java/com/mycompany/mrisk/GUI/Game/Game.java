978ef01fa1c3eb641c84778a21927228376ca964
joonas <jonsterivilja@hotmail.com>
2018-11-17 11:00:53 +0200

    Created initial structure and files for MRisk

---
+++ mrisk/MRisk/src/main/java/com/mycompany/mrisk/GUI/Game/Game.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mrisk.GUI.Game;
+
+import GUI.StartScreen;
+
+/**
+ *
+ * @author joonas
+ */
+public class Game {
+    
+    public static void main(String[] args) {
+        //System.out.println("Starting...");
+        //StartScreen s = new StartScreen();
+        
+        //s.
+        
+    }
+}
