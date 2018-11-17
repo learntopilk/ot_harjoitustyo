1080f15d7559a9f1e18da036ee84af8b408495ab
joonas <jonsterivilja@hotmail.com>
2018-11-17 11:43:21 +0200

    Basic GUI for start screen

--- dokumentointi/tyoaikakirjanpito.md
+++ dokumentointi/tyoaikakirjanpito.md
@@ -2,4 +2,5 @@
 | päivä | Aika| Mitä oikein duunasin|
 |--:|:-:|:--|
 | 13.11.2018 | 2h | Projektin suunnittelu ja vaatimusmäärittelyä |
+|17.11.2018|| Projektin rakenne, JavaFX:n opettelua ja GUI:n rakentelua |
 
--- mrisk/MRisk/src/main/java/GUI/StartScreen.java
+++ mrisk/MRisk/src/main/java/GUI/StartScreen.java
@@ -9,8 +9,10 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -39,16 +41,76 @@ public class StartScreen extends Application{
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
+        Button startGame = new Button();
+        startGame.setText("Start Game");
+        startGame.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                System.out.println("Starting game...");
+            }
+        });
+        
+        Button quitGame = new Button();
+        quitGame.setText("Quit game");
+        quitGame.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                System.out.println("Quitting game...");
+                quit();
+            }
+        });
+        
+        Button seeStats = new Button();
+        seeStats.setText("Statistics");
+        seeStats.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle (ActionEvent event) {
+                System.out.println("Opening statistics...");
+            }
+        });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(20);
+        grid.setVgap(20);
+        
+        grid.add(startGame, 0, 0);
+        grid.add(btn, 0, 1);
+        grid.add(seeStats, 0, 2);
+        grid.add(quitGame, 0, 3);
+        
+        Scene startScene = new Scene(grid, 700, 500);
+        
+        //root.getChildren().add(btn);
+        //root.getChildren().add(startGame);
+        
+        //root.autosize();
+        
+        //primaryStage.setScene(new Scene(root, 600, 800));
+        primaryStage.setScene(startScene);
         primaryStage.show();
+    }
+    
+    private void goToGameScreen() {
+        
+    }
+    
+    private void goToStartScreen() {
+        
+    }
+    
+    private void goToStats() {
+        
+    }
+    
+    private void quit() {
+        // TODO: kutsu tallennusta?
+        System.exit(0);
     }
 }
