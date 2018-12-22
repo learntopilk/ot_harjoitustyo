737149f77b33fa3fc683d441bd12efb020ea5bd9
joonas <jonsterivilja@hotmail.com>
2018-12-22 21:51:36 +0200

    Removed unnecessary comments

--- mrisk/MRisk/src/main/java/gui/StartScreen.java
+++ mrisk/MRisk/src/main/java/gui/StartScreen.java
@@ -63,9 +63,6 @@ public class StartScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        //Image imgur = new Image(getClass().getClassLoader().getResource("f1.png").getFile());
-        //bg = new BackgroundImage(new Image("/f1.png", screenHeight, screenWidth, false, true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
-        //String URI = getClass().getClassLoader().getClass().getResource("./f1.png").getPath();
         bg = new BackgroundImage(new Image("resources/f1.png", 
                 screenHeight, screenWidth, false, true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
 
